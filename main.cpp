34650879c5eada0135177d82e63205f495fd8890
Eldar Kusdavletov <eldar.kusd@gmail.com>
2025-07-06 14:41:40 +0500

    added multithreading test

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
         MarketEvent event = feed.generate_event();
         processor.process_event(event);
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 
     processor.print_summary();
---
+++ tests/test_market_data_processor.cpp
@@ -0,0 +1,40 @@
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include "../include/MarketDataProcessor.hpp"
+
+class MarketDataProcessorTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        processor = std::make_unique<MarketDataProcessor>();
+    }
+
+    void TearDown() override {
+        processor.reset();
+    }
+
+    std::unique_ptr<MarketDataProcessor> processor;
+};
+
+TEST_F(MarketDataProcessorTest, ConcurrentProcessing) {
+    const int numThreads = 4;
+    const int eventsPerThread = 100;
+    std::vector<std::thread> threads;
+
+    threads.reserve(numThreads);
+    for (int t = 0; t < numThreads; ++t) {
+        threads.emplace_back([this, t]() {
+            for (int i = 0; i < eventsPerThread; ++i) {
+                MarketEvent event = MarketEvent("SYMBOL " + std::to_string(t), 100.0 + i, 1000 + i, EventType::TRADE);
+                processor->process_event(event);
+            }
+        });
+    }
+
+    for (auto &thread: threads) {
+        thread.join();
+    }
+
+    SUCCEED();
+}
